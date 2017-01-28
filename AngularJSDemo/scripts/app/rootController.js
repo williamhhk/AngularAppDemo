1a25f7e48e89c92e4d60bdc6d805a426568bcc2b
williamhhk <william.han@gmail.com>
2017-01-28 06:17:18 -0700

    Clean up

--- AngularJSDemo/scripts/app/rootController.js
+++ AngularJSDemo/scripts/app/rootController.js
@@ -146,24 +146,6 @@
                 })
             })
         }
-
-        //function Merge(target, source) {
-        //    for (var p in source) {
-        //        try {
-        //            // Property in destination object set; update its value.
-        //            if (source[p].constructor == Object) {
-        //                target[p] = MergeRecursive(target[p], source[p]);
-
-        //            } else {
-        //                target[p] = source[p];
-        //            }
-        //        } catch (e) {
-        //            // Property in destination object not set; create it and set its value.
-        //            target[p] = source[p];
-        //        }
-        //    }
-        //    return target;
-        //}
     }
 })(angular.module('myApp'));
 
